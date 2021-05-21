4356c2c19f7570e2ff76dd2d51b3e8b3e138a964
Hongce Chen <natechc1@my.yorku.ca>
2021-05-21 17:27:20 -0400

    Update courseSystem.c

--- courseSystem/courseSystem.c
+++ courseSystem/courseSystem.c
@@ -1,18 +1,4 @@
-/* PLEASE DO NOT MODIFY A SINGLE STATEMENT IN THE TEXT BELOW.
-READ THE FOLLOWING CAREFULLY AND FILL IN THE GAPS
-
-I hereby declare that all the work that was required to 
-solve the following problem including designing the algorithms
-and writing the code below, is solely my own and that I received
-no help in creating this solution and I have not discussed my solution 
-with anybody. I affirm that I have read and understood
-the Senate Policy on Academic honesty at 
-https://secretariat-policies.info.yorku.ca/policies/academic-honesty-senate-policy-on/
-and I am well aware of the seriousness of the matter and the penalties that I will face as a 
-result of committing plagiarism in this assignment.
-
-BY FILLING THE GAPS,YOU ARE SIGNING THE ABOVE STATEMENTS.
-
+/* 
 Full Name: Hongce Chen
 Student Number: 216792327
 Course Section: M
